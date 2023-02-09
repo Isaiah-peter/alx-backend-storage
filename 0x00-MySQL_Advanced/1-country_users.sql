8c6652a63ec8d0e84ca731f6d026cd155811b7d6
Isaiah peter <peterisaiah4fun@gmail.com>
2023-02-09 23:28:30 +0100

    task 2

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,10 @@
 --creates a table users
---field includes name, email id
+--field includes name, email id, country
 
 CREATE TABLE IF NOT EXISTS user(
   id int NOT NULL AUTO_INCREMENT,
   email varchar(255) NOT NULL UNIQUE,
   name varchar(255),
+  country enum('US', 'CO', 'TN') DEFAULT 'US',
   PRIMARY KEY(id)
 );
